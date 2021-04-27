462e67640c86b40e806b81e7bd3204335108c765
triki96 <giovannitognolini@gmail.com>
2021-04-27 17:31:21 +0200

    seminari recenti futuri passati

--- src/app/gruppi/cifris-cloud/cifris-cloud.component.ts
+++ src/app/gruppi/cifris-cloud/cifris-cloud.component.ts
@@ -22,4 +22,16 @@ export class CifrisCloudComponent implements OnInit {
     } catch (e) { }
   }
 
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  /* Ai posteri: utilizzare del codice asincrono (async/await) mi è sembrato l'unico 
+  modo per gestire la transizione della pagina alla sezione "eventi passati".*/
+  async goTo(s : string){
+    /* this.attivitaDaMostrare = s; */
+    await this.sleep(10);
+    document.getElementById(s).scrollIntoView({behavior: 'smooth'});
+  }
+
 }
