e297eb0ef818d27436dd186df0bbc05115b6913c
triki96 <giovannitognolini@gmail.com>
2021-06-25 11:24:42 +0200

    nuovi seminari

--- src/app/attivita/gare-crittografiche/cryptowars/cryptowars.component.ts
+++ src/app/attivita/gare-crittografiche/cryptowars/cryptowars.component.ts
@@ -19,8 +19,8 @@ export class CryptowarsComponent implements OnInit {
   /* Ai posteri: utilizzare del codice asincrono (async/await) mi è sembrato l'unico 
   modo per gestire la transizione della pagina alla sezione "eventi passati".*/
   async goTo(s : string){
-    /* await this.sleep(10);
-    document.getElementById(s).scrollIntoView({behavior: 'smooth'}); */
+    await this.sleep(10);
+    document.getElementById(s).scrollIntoView({behavior: 'smooth'});
   }
 
 }
