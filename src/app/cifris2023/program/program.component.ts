6d966d2b7e48d55ffed5015f7f40b41ec386ef5a
danieledebernardini <debernardinidani@gmail.com>
2023-10-20 13:08:44 +0200

    Added Show More/Show Less functionality for abstracts in cifris23/program.

--- src/app/cifris2023/program/program.component.ts
+++ src/app/cifris2023/program/program.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-program',
-  templateUrl: './program.component.html',
-  styleUrls: ['./program.component.css']
+	selector: 'app-program',
+	templateUrl: './program.component.html',
+	styleUrls: ['./program.component.css']
 })
 export class ProgramComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	public readMoreHandler(event) {
+		const button = event.currentTarget;
+
+		const targetId = button.getAttribute('targetId')
+		const abstract = document.querySelector('#' + targetId);
+		
+		abstract.classList.toggle('expanded');
+		button.textContent = 	
+			abstract.classList.contains('expanded') ? 'Read less' : 'Read more';
+	}
 }
