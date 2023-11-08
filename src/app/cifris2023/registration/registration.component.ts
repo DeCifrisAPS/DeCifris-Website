898216fedb4689bb9b8e482cdfe9cf8a7c45d787
danieledebernardini <debernardinidani@gmail.com>
2023-11-08 14:31:52 +0100

    Modified parameters for enabling payment system.

--- src/app/cifris2023/registration/registration.component.ts
+++ src/app/cifris2023/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto-js';
 
-import { formatDate } from "@angular/common";
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -17,16 +17,18 @@ export class RegistrationComponent implements OnInit {
 	//---------------------------------------------------------------------------
 	//---------------------------- Payment variables ----------------------------
 
-	// Alias e chiave segreta
-	readonly ALIAS = "ALIAS_WEB_00074470";
-	readonly CHIAVESEGRETA = "SQV946OD2KUQ4M71SHXBCB85SW3FVVQF";
+	//readonly ALIAS = 'ALIAS_WEB_00074470';
+	readonly ALIAS = 'payment_3482210';	
 
-	readonly HTTP_HOST = "localhost:4200/cifris23/";
-	//readonly HTTP_HOST = "www.decifris.it/cifris23/";
+	//readonly CHIAVESEGRETA = 'SQV946OD2KUQ4M71SHXBCB85SW3FVVQF';
+	readonly CHIAVESEGRETA = 'D68kw33a4HE9Q7352HY30v3M9kV3O50e6A2542W9';
 
-	//readonly requestUrl = 'https://ecommerce.nexi.it/' +
+	//readonly HTTP_HOST = 'localhost:4200/cifris23/';
+	readonly HTTP_HOST = 'www.decifris.it/cifris23/';
+
+	//readonly requestUrl = 'https://int-ecommerce.nexi.it/' +
 	//						'ecomm/ecomm/DispatcherServlet';
-	readonly requestUrl = 'https://int-ecommerce.nexi.it/' +
+	readonly requestUrl = 'https://ecommerce.nexi.it/' +
 						  'ecomm/ecomm/DispatcherServlet';
 
 	readonly DIVISA = 'EUR';
@@ -59,34 +61,30 @@ export class RegistrationComponent implements OnInit {
 
 	/**
 	 * Instantiates a payment issue.
-	 * 
-	 * TODO:
-	 * - Comments correspond to the production phase.
 	 */
 	public startPayment() {
 	
 		// Data preprocessing
-		//var merchantServerUrl = "https://" + this.HTTP_HOST;
-		var merchantServerUrl =	'http://' + this.HTTP_HOST;
+		//var merchantServerUrl =	'http://' + this.HTTP_HOST;
+		var merchantServerUrl = 'https://' + this.HTTP_HOST;
 
 		var date = new Date();
-		var codTrans = "TESTPS_" +
-					   formatDate(date, 'yyyyMMddHHmmss', 'en-US');
+		var codTrans = 'PS_' + formatDate(date, 'yyyyMMddHHmmss', 'en-US');
 
 		var importo = this.fee;
 
-		var macString = "codTrans=" + codTrans +
-						"divisa=" + this.DIVISA +
-						"importo=" + importo +
+		var macString = 'codTrans=' + codTrans +
+						'divisa=' + this.DIVISA +
+						'importo=' + importo +
 						this.CHIAVESEGRETA;
 		var macCalculated = this.hashMac(macString);
 
 		// Filling hidden inputs
-		var importoIn = <HTMLInputElement> document.getElementById("importo");
-		var codTransIn = <HTMLInputElement> document.getElementById("codTrans");
-		var urlIn = <HTMLInputElement> document.getElementById("url");
-		var urlBackIn = <HTMLInputElement> document.getElementById("url_back");
-		var macIn = <HTMLInputElement> document.getElementById("mac");
+		var importoIn = <HTMLInputElement> document.getElementById('importo');
+		var codTransIn = <HTMLInputElement> document.getElementById('codTrans');
+		var urlIn = <HTMLInputElement> document.getElementById('url');
+		var urlBackIn = <HTMLInputElement> document.getElementById('url_back');
+		var macIn = <HTMLInputElement> document.getElementById('mac');
 
 		importoIn.value = importo.toString();
 		codTransIn.value = codTrans;
@@ -95,7 +93,7 @@ export class RegistrationComponent implements OnInit {
 		macIn.value = macCalculated;
 
 		// Form submission
-		var form = <HTMLFormElement> document.getElementById("payForm");
+		var form = <HTMLFormElement> document.getElementById('payForm');
 		form.action = this.requestUrl;
 		form.submit();
 	}
