e8e859f32ad62c5cada0ac2721c640f4cb5fa5cd
danieledebernardini <debernardinidani@gmail.com>
2024-03-07 19:11:46 +0100

    Minor changes in attivita/trends24

--- src/app/attivita/trends24/trends24.component.ts
+++ src/app/attivita/trends24/trends24.component.ts
@@ -12,4 +12,16 @@ export class Trends24Component implements OnInit {
 	ngOnInit(): void {
 	}
 
+	sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	/* Ai posteri: utilizzare del codice asincrono (async/await) mi è sembrato l'unico 
+	modo per gestire la transizione della pagina alla sezione "eventi passati".*/
+	async goTo(s : string){
+		/* this.attivitaDaMostrare = s; */
+		/* this.mostraAttivita(s); */
+		await this.sleep(10);
+		document.getElementById(s).scrollIntoView({behavior: 'smooth'});
+	}
 }
