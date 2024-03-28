ef1a98c77398378e236d478fe004b1c712bdcd75
danieledebernardini <debernardinidani@gmail.com>
2024-03-28 13:48:02 +0100

    Implemented the front-end structure of ./attivita/editoria/koine/articles-and-volumes.

--- src/app/attivita/editoria/koine/articles-and-volumes/articles-and-volumes.component.ts
+++ src/app/attivita/editoria/koine/articles-and-volumes/articles-and-volumes.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
-  selector: 'app-articles-and-volumes',
-  templateUrl: './articles-and-volumes.component.html',
-  styleUrls: ['./articles-and-volumes.component.css']
+	selector: 'app-articles-and-volumes',
+	templateUrl: './articles-and-volumes.component.html',
+	styleUrls: ['./articles-and-volumes.component.css']
 })
 export class ArticlesAndVolumesComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	public openCollapsible(event) {
+
+		const button = <HTMLElement> event.currentTarget;
+		const volume = <HTMLElement> button.nextElementSibling;
+
+		volume.classList.toggle('show');
+
+		// Adding an arrow to the right of the button
+		const arrow = <HTMLElement> button.querySelector('.arrow');
+		arrow.textContent = 
+			volume.classList.contains('show') ? '\u25B2' : '\u25BC';
+	}
 }
