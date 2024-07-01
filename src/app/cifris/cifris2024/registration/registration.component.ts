7c9e95d01288d1069533581c978d50ff04e10300
PlasmaStark <leonardo.errati99@gmail.com>
2024-07-01 17:36:06 +0200

    Sito aggiornato per pagamenti CIFRIS24. Fix stilistici minori.

--- src/app/cifris/cifris2024/registration/registration.component.ts
+++ src/app/cifris/cifris2024/registration/registration.component.ts
@@ -11,14 +11,14 @@ import { NgForm } from '@angular/forms';
 	styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent24 implements OnInit {
-	
+
 	public fee = 39000;
 
 	readonly prices = [
-						[35000, 30000, 25000, 16000, 0],
-						[26000, 24000, 20000, 13000, 0],
-						[15000, 12000, 11000, 6000, 0]
-					]
+		[35000, 30000, 25000, 16000, 0],
+		[26000, 24000, 20000, 13000, 0],
+		[15000, 12000, 11000, 6000, 0]
+	]
 
 	//---------------------------------------------------------------------------
 	//---------------------------- Payment variables ----------------------------
@@ -33,19 +33,19 @@ export class RegistrationComponent24 implements OnInit {
 	*/
 
 	// PRODUCTION PHASE
-	readonly ALIAS = 'payment_3482210';	
+	readonly ALIAS = 'payment_3482210';
 	readonly CHIAVESEGRETA = 'D68kw33a4HE9Q7352HY30v3M9kV3O50e6A2542W9';
 	readonly HTTP_HOST = 'www.decifris.it/cifris24/';
 	readonly requestUrl = 'https://ecommerce.nexi.it/' +
-						  'ecomm/ecomm/DispatcherServlet';
+		'ecomm/ecomm/DispatcherServlet';
 
 	readonly DIVISA = 'EUR';
 
 	//---------------------------------------------------------------------------
 
-	constructor() {}
+	constructor() { }
 
-	ngOnInit(): void {}
+	ngOnInit(): void { }
 
 	/**
 	 * Check the status of input 'dinner' eventually adding/subtracting the
@@ -54,7 +54,7 @@ export class RegistrationComponent24 implements OnInit {
 	public checkDinner(event) {
 
 		const checkButton = event.currentTarget;
-		
+
 		var value;
 
 		if (checkButton.checked) {
@@ -71,8 +71,8 @@ export class RegistrationComponent24 implements OnInit {
 	 * dinner must be checked and disabled.
 	 */
 	public companionFee() {
-		
-		const dinner = <HTMLInputElement> document.getElementById('dinner');
+
+		const dinner = <HTMLInputElement>document.getElementById('dinner');
 
 		// Checking and disabling dinner checkbox
 		dinner.checked = true;
@@ -92,7 +92,7 @@ export class RegistrationComponent24 implements OnInit {
 		// Checking checked participation radio button
 		var radio = document.getElementsByName('participation');
 		for (var i = 0; i < radio.length; i++) {
-			if ((<HTMLInputElement> radio[i]).checked) {
+			if ((<HTMLInputElement>radio[i]).checked) {
 				r = i;
 			}
 		}
@@ -100,7 +100,7 @@ export class RegistrationComponent24 implements OnInit {
 		// Checking checked fee radio button
 		radio = document.getElementsByName('fee');
 		for (i = 0; i < radio.length; i++) {
-			if ((<HTMLInputElement> radio[i]).checked) {
+			if ((<HTMLInputElement>radio[i]).checked) {
 				c = i;
 			}
 		}
@@ -108,7 +108,7 @@ export class RegistrationComponent24 implements OnInit {
 		this.fee = this.prices[r][c];
 
 		// Enabling dinner checkbox and eventually adding it
-		const dinner = <HTMLInputElement> document.getElementById('dinner');
+		const dinner = <HTMLInputElement>document.getElementById('dinner');
 		dinner.disabled = false;
 		if (dinner.checked) {
 			this.fee += 5000;
@@ -120,9 +120,19 @@ export class RegistrationComponent24 implements OnInit {
 	 */
 	public setFee() {
 
-		const companion = <HTMLInputElement> document.getElementById('companion');
+		// Activating all participation buttons (ALL, WORKOSHOP,...)
+		var radio = document.getElementsByName('participation');
+		for (var i = 0; i < radio.length; i++) {
+			(<HTMLInputElement>radio[i]).disabled = false;
+		}
+		const companion = <HTMLInputElement>document.getElementById('companion');
 
 		if (companion.checked) {
+			// Deactivating all participation buttons (ALL, WORKOSHOP,...)
+			var radio = document.getElementsByName('participation');
+			for (var i = 0; i < radio.length; i++) {
+				(<HTMLInputElement>radio[i]).disabled = true;
+			}
 			this.companionFee();
 		} else {
 			this.participantFee();
@@ -143,30 +153,30 @@ export class RegistrationComponent24 implements OnInit {
 	 * Instantiates a payment issue.
 	 */
 	public startPayment() {
-	
+
 		// Data preprocessing
 		//var merchantServerUrl =	'http://' + this.HTTP_HOST;
 		var merchantServerUrl = 'https://' + this.HTTP_HOST;
 
 		var date = new Date();
 		var codTrans = 'CIFRIS24_'
-						+ formatDate(date, 'yyyyMMddHHmmss', 'en-US');
+			+ formatDate(date, 'yyyyMMddHHmmss', 'en-US');
 
 		var importo = this.fee;
 
 		var macString = 'codTrans=' + codTrans +
-						'divisa=' + this.DIVISA +
-						'importo=' + importo +
-						this.CHIAVESEGRETA;
+			'divisa=' + this.DIVISA +
+			'importo=' + importo +
+			this.CHIAVESEGRETA;
 		var macCalculated = this.hashMac(macString);
 
 		// Filling hidden inputs
-		var importoIn = <HTMLInputElement> document.getElementById('importo');
-		var codTransIn = <HTMLInputElement> document.getElementById('codTrans');
-		var urlIn = <HTMLInputElement> document.getElementById('url');
-		var urlBackIn = <HTMLInputElement> document.getElementById('url_back');
-		var macIn = <HTMLInputElement> document.getElementById('mac');
-		var descrizione = <HTMLInputElement> document.getElementById('descrizione');
+		var importoIn = <HTMLInputElement>document.getElementById('importo');
+		var codTransIn = <HTMLInputElement>document.getElementById('codTrans');
+		var urlIn = <HTMLInputElement>document.getElementById('url');
+		var urlBackIn = <HTMLInputElement>document.getElementById('url_back');
+		var macIn = <HTMLInputElement>document.getElementById('mac');
+		var descrizione = <HTMLInputElement>document.getElementById('descrizione');
 
 		importoIn.value = importo.toString();
 		codTransIn.value = codTrans;
@@ -182,7 +192,7 @@ export class RegistrationComponent24 implements OnInit {
 		}
 
 		// Form submission
-		var form = <HTMLFormElement> document.getElementById('payForm');
+		var form = <HTMLFormElement>document.getElementById('payForm');
 		form.action = this.requestUrl;
 		form.submit();
 	}
@@ -191,22 +201,22 @@ export class RegistrationComponent24 implements OnInit {
 	 * Disables the form fields.
 	 */
 	public setDisabled() {
-		
+
 		// Disabling fee radio buttons 
 		var radio = document.getElementsByName('fee');
 		for (var i = 0; i < radio.length; i++) {
-			(<HTMLInputElement> radio[i]).disabled = true;
+			(<HTMLInputElement>radio[i]).disabled = true;
 		}
 
 		// Disabling participation radio buttons 
 		radio = document.getElementsByName('participation');
 		for (var i = 0; i < radio.length; i++) {
-			(<HTMLInputElement> radio[i]).disabled = true;
+			(<HTMLInputElement>radio[i]).disabled = true;
 		}
 
 		// Disabling dinner checkbox button
 		var checkbox = document.getElementById('dinner');
-		(<HTMLInputElement> checkbox).disabled = true;
+		(<HTMLInputElement>checkbox).disabled = true;
 	}
 
 	/**
@@ -216,17 +226,17 @@ export class RegistrationComponent24 implements OnInit {
 	 */
 	public checkAndPay(): boolean {
 		// Checking form fields
-		var form = <HTMLFormElement> document.getElementById('payForm');
+		var form = <HTMLFormElement>document.getElementById('payForm');
 		var isValid = form.checkValidity();
 
-		if(isValid) {
+		if (isValid) {
 			// Disbaling fields and executing payment
 			this.setDisabled();
 			this.startPayment();
 		} else {
 			// Throwing error
-			alert('Fields with * are mandatory. \n' + 
-				  'Please be sure to have compiled all of them.'); 
+			alert('Fields with * are mandatory. \n' +
+				'Please be sure to have compiled all of them.');
 		}
 
 		return isValid;
