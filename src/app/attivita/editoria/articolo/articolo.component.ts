f8975850529962f3d407b486889e1a82e9cd5395
1dotd4 <15785841+1dotd4@users.noreply.github.com>
2024-08-22 17:08:42 +0200

    Add some notes

--- src/app/attivita/editoria/articolo/articolo.component.ts
+++ src/app/attivita/editoria/articolo/articolo.component.ts
@@ -25,6 +25,66 @@ export class ArticoloComponent {
         const articleReq = params.get('article');
         this.theVolume = data.find((v) => v.id === volumeReq);
         this.theArticle = this.theVolume.articles.find((a) => a.id === articleReq);
+        /* Indexing, inclusion, crawler and more
+         *
+         * https://web.archive.org/web/20240420083327/https://help.scholasticahq.com/article/214-how-does-google-scholar-indexing-work-on-scholastica
+         *
+         * * Google Scholar
+         * https://scholar.google.com/intl/it/scholar/inclusion.html
+         * Make sure that:
+         * a. the full text of your paper is in a PDF file that ends with ".pdf",
+         * b. the title of the paper appears in a large font on top of the first page,
+         * c. the authors of the paper are listed right below the title on a separate line, and
+         * d. there's a bibliography section titled, e.g., "References" or "Bibliography" at the end. 
+         *
+         * https://www.oajournals-toolkit.org/indexing/journal-and-article-indexing
+         *
+         * https://www.centre-mersenne.org/en/indexing-your-journal-in-scopus/
+         * https://www.elsevier.com/products/scopus/content/content-policy-and-selection
+         * https://blog.scholasticahq.com/post/how-to-get-journals-indexed-scopus/
+         * https://guides.lib.ku.edu/journal_editors/launching
+         * https://blog.scholasticahq.com/post/getting-journal-indexed-in-scopus-case-study/
+         */
+
+        /* * Tags to populate
+         *
+         * * Following Twitter meta
+         * twitter:site
+         * twitter:card
+         * twitter:title
+         * twitter:description
+         * twitter:image
+         *
+         * * Google Scholar (?)
+         * citation_pdf_url
+         * citation_fulltext_html_url
+         * citation_abstract_html_url
+         * citation_inbook_title
+         * citation_title
+         * citation_publication_date
+         * citation_firstpage
+         * citation_lastpage
+         * citation_doi
+         * citation_issn
+         * citation_isbn
+         * citation_conference_series_id
+         * citation_conference_title
+         * size (?)
+         * description
+         * citation_author
+         * citation_author_email
+         * citation_author_institution
+         * citation_publisher
+         * 
+         * * Open Graph meta
+         * og:url
+         * og:type
+         * og:site_name
+         * og:title
+         * og:description
+         * og:image
+         *
+         */
         // this.metaService.addTag({ name: 'description', content: 'Article number ' + this.articleId + '!' });
       });
     });
