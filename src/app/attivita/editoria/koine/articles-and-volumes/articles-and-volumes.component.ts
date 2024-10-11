bbd8b4f21d1393c43a419c3a793eaec9d5da34a9
PlasmaStark <leonardo.errati99@gmail.com>
2024-10-11 15:26:15 +0200

    Aggiornamento grafica Koine.

--- src/app/attivita/editoria/koine/articles-and-volumes/articles-and-volumes.component.ts
+++ src/app/attivita/editoria/koine/articles-and-volumes/articles-and-volumes.component.ts
@@ -12,17 +12,4 @@ export class ArticlesAndVolumesComponent implements OnInit {
 
 	ngOnInit(): void {
 	}
-
-	public openCollapsible(event) {
-
-		const button = <HTMLElement> event.currentTarget;
-		const volume = <HTMLElement> button.nextElementSibling;
-
-		volume.classList.toggle('show');
-
-		// Adding an arrow to the right of the button
-		const arrow = <HTMLElement> button.querySelector('.arrow');
-		arrow.textContent = 
-			volume.classList.contains('show') ? '\u25B2' : '\u25BC';
-	}
 }
