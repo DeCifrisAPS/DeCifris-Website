2c19ef907afc9480a79e5b7131daf9023d54a23e
1dotd4 <15785841+1dotd4@users.noreply.github.com>
2024-10-19 17:43:10 +0200

    Volume 4 generato da json

--- src/app/attivita/editoria/koine/vol4/vol4.component.ts
+++ src/app/attivita/editoria/koine/vol4/vol4.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
+import { ArticoloService } from '../../articolo/articolo.service';
+import { Volume, Articolo, Author } from '../../articolo/articolo.model';
 
 @Component({
   selector: 'app-vol4',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: '../../../../../styles-koine-volumes.css'
 })
 export class Vol4Component {
+
+  theVolume: Volume = { id: "", title: "", publisher: "", published: "", series: "", ISBN: "", ISSN: "", volumeLink: "", coverLink: "", articles: [] };
+
+  constructor(private metaService: Meta, private titleService: Title, private articoloService: ArticoloService) {}
+
+  ngOnInit() {
+    this.articoloService.getVolume("koine", "vol4").subscribe((volume: Volume)  => {
+      this.theVolume = volume;
+    });
+  }
 
 }
